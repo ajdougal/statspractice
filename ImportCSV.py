dbad16934c18e99f9ba22b5ae47819a753d00b6e
Alex Dougal <adougal@rmn.com>
2019-01-25 22:18:36 -0600

    Count number of turnovers in a point and print

--- ImportCSV.py
+++ ImportCSV.py
@@ -7,9 +7,21 @@ game_date_enum = set()
 team_dict = dict()
 point_dict = dict()
 line_count = 0
+action_enum = set()
+
+
+def count_number_of_turnovers(point):
+    turnovers = 0
+    for inner_event in point:
+        if inner_event[8] in ("Drop" or "Stall" or "Throwaway"):
+            turnovers += 1
+    return turnovers
+
+
 with file("AustinSol2018-stats.csv") as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
+        action_enum.add(row[8])
         if line_count is not 0:
             row_dictionary.append(row)
             team_enum.add(row[2])
@@ -24,7 +36,6 @@ with file("AustinSol2018-stats.csv") as csv_file:
 
         line_count += 1
 
-print len(team_dict['Atlanta Hustle']['2018-05-19 19:06'])
 previous_home = 0
 previous_away = 0
 point_counter = 0
@@ -37,5 +48,9 @@ for event in team_dict['Atlanta Hustle']['2018-05-19 19:06']:
     point_dict['Atlanta Hustle']['2018-05-19 19:06'][point_counter].append(event)
     previous_home = int(event[5])
     previous_away = int(event[6])
-pprint.pprint(point_dict, depth=4)
+
+for point_iterator in range(1, 1+len(point_dict['Atlanta Hustle']['2018-05-19 19:06'])):
+    point = point_dict['Atlanta Hustle']['2018-05-19 19:06'][point_iterator]
+    print count_number_of_turnovers(point)
+
 
